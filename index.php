65faaef77e75a36fc01b858204ab305ff601bf3c
Thiago kauai <html5ephp@gmail.com>
2019-05-04 23:38:15 -0300

    inicializando testes com slim framework

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require('vendor/autoload.php');
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+$request = function ($request,$response,$args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    dump($request);
+    return $response;
+};
+
+$app->get('/hello/{name}',$request);
+
+$app->run();
